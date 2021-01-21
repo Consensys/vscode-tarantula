830677161f039463e22795c2fb5ca67593134764
tintinweb <tintinweb@oststrom.com>
2021-01-21 16:01:06 +0100

    update light/dark highlighting

--- src/features/decorator.js
+++ src/features/decorator.js
@@ -7,52 +7,13 @@
  * */
 const vscode = require('vscode');
 
-function HSLtoRGB(h, s, l) {
-    let r, g, b;
-
-    const rd = (a) => {
-        return Math.floor(Math.max(Math.min(a * 256, 255), 0));
-    };
-
-    const hueToRGB = (m, n, o) => {
-        if (o < 0) {
-            o += 1;
-        }
-        if (o > 1) {
-            o -= 1;
-        }
-        if (o < 1 / 6) {
-            return m + (n - m) * 6 * o;
-        }
-        if (o < 1 / 2) {
-            return n;
-        }
-        if (o < 2 / 3) {
-            return m + (n - m) * (2 / 3 - o) * 6;
-        }
-        return m;
-    };
-
-    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    const p = 2 * l - q;
-
-    r = hueToRGB(p, q, h + 1 / 3);
-    g = hueToRGB(p, q, h);
-    b = hueToRGB(p, q, h - 1 / 3);
-
-    return [rd(r), rd(g), rd(b)];
-}
-
-function RGBtoHex(r, g, b) {
-    return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
-}
-
 const lineDecoSettings = {
     prefix: "hue",
     resolution: 10,
     alphaStart: 0,
-    alphaEnd: 60
-}
+    alphaEnd: 60,
+    backgroundColor: {light:"E86252", dark:"E9190F"}
+};
 
 const decoStyle = {
     ... {
@@ -62,11 +23,11 @@ const decoStyle = {
             overviewRulerLane: vscode.OverviewRulerLane.Right,
             light: {
                 // this color will be used in light color themes
-                backgroundColor: RGBtoHex(...HSLtoRGB(((5 + 5) * 19) % 255 / 255, 0.85, 0.75)) + "50"
+                backgroundColor: `#${lineDecoSettings.backgroundColor.light}50`
             },
             dark: {
                 // this color will be used in dark color themes
-                backgroundColor: RGBtoHex(...HSLtoRGB(((5 + 5) * 19) % 255 / 255, 0.85, 0.75)) + "50"
+                backgroundColor: `#${lineDecoSettings.backgroundColor.dark}50`
             },
         }),
     },
@@ -99,6 +60,7 @@ function createDecoStyle(prefix, num, start, end){
 
     let step = Math.abs((end-start)/num);
 
+
     for(let i=start; i<=end; i+=step){
         result[`${prefix}_${i}`] =  vscode.window.createTextEditorDecorationType({
                 
@@ -107,11 +69,11 @@ function createDecoStyle(prefix, num, start, end){
                 overviewRulerLane: vscode.OverviewRulerLane.Right,
                 light: {
                     // this color will be used in light color themes
-                    backgroundColor: RGBtoHex(...HSLtoRGB(((5 + 5) * 19) % 255 / 255, 0.85, 0.75)) + `${i}`
+                    backgroundColor: `#${lineDecoSettings.backgroundColor.light}${i.toString(16)}`
                 },
                 dark: {
                     // this color will be used in dark color themes
-                    backgroundColor: RGBtoHex(...HSLtoRGB(((5 + 5) * 19) % 255 / 255, 0.85, 0.75)) + `${i}`
+                    backgroundColor: `#${lineDecoSettings.backgroundColor.dark}${i.toString(16)}`
                 },
             });
     }
